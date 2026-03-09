7671c76c9d33d28f3a585bea94ba2e9fa5d3b37f
Jesse <jesse.postma3108@gmail.com>
2026-03-09 08:56:40 +0100

    usage pagina

--- web/app.py
+++ web/app.py
@@ -11,5 +11,10 @@ def about():
     return render_template('about.html')
 
 
+@app.route("/how-to")
+def usage():
+    return render_template('usage.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)

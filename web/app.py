0e00d6b7c9007c7c3f63087142441b0b0aeeb0de
RobSob27 <robinoffringa27@gmail.com>
2026-03-10 13:04:51 +0100

    titels

--- web/app.py
+++ web/app.py
@@ -2,17 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def index(title="Pipeline"):
     return render_template('index.html')
 
 
 @app.route("/about")
-def about():
+def about(title="About"):
     return render_template('about.html')
 
 
 @app.route("/how-to")
-def usage():
+def usage(title="Usage Instructions"):
     return render_template('usage.html')
 
 

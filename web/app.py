05bb93ddeb6859b1fcf54c24fcb4ac108a534fd9
Jessep05 <jesse.postma3108@gmail.com>
2026-03-12 16:58:51 +0100

    final fixes

--- web/app.py
+++ web/app.py
@@ -7,9 +7,9 @@ def index():
     configurations = []
     if request.method == "POST":
         kwargs = {
-            "kraken_quick": bool(request.form.get("kraken-quick")),
-            "kraken_confidence": float(request.form.get("kraken-confidence")),
-            "kraken_threads": int(request.form.get("kraken-threads"))
+            "kraken_quick": request.form.get("kraken-quick", type=bool),
+            "kraken_confidence": request.form.get("kraken-confidence", type=float),
+            "kraken_threads": request.form.get("kraken-threads", type=int),
         }
         configurations = [f"{key}: {value}" for key, value in kwargs.items()]
     return render_template('index.html', title="Pipeline", configurations=configurations)

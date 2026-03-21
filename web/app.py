0a7aed03e335fc513125d0484c79bdc6e9061054
JesseP05 <jesse.postma3108@gmail.com>
2026-03-21 17:55:54 +0100

    job class, flask config serializaton, job saving, refactors

--- web/app.py
+++ web/app.py
@@ -1,9 +1,52 @@
+
+
+import json
+import time
+import os
 from flask import Flask, render_template, request
+from shared.models import Job
+
+
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        job_id = time.time_ns()
+
+        fastq_file = request.files.get('fastq-file') or None
+        fastq_name = fastq_file.name
+
+        job_dir = f"shared/data/{job_id}"
+        os.makedirs(job_dir, exist_ok=True)
+        
+        fastq_path = f"{job_dir}/{fastq_name}.fastq"
+        fastq_file.save(fastq_path)
+
+        fastp_kwargs = {
+            "min-quality": request.form.get("fastp-quality", 15),
+            "trim-adapters": request.form.get("fastp-adapter", 1),
+            "length-min": request.form.get("fastp-min-length", 60),
+            "length-max": request.form.get("fastp-length-max", 0),
+            "cut-front": request.form.get("fastp-cut-front", 0),
+            "cut-tail": request.form.get("fastp-cut-tail", 0),
+            "deduplicate": request.form.get("fastp-dedup", 0),
+        }
+
+        kraken_kwargs = {
+            "confidence": request.form.get("kraken-confidence", 0.0),
+            "paired-end": request.form.get("kraken-paired", 0),
+            "use-science-names": request.form.get("kraken-use-names", 0),
+            "base-quality": request.form.get("kraken-base-quality", 0),
+        }
+
+        fastp = Job.FastP(**fastp_kwargs)
+        kraken = Job.Kraken(**kraken_kwargs)
+        job = Job(job_id, fastq_path, "pending", fastp=fastp, kraken=kraken)
+        job.create_commands()
+        job.save()
+
     return render_template('index.html', title="Pipeline")
 
 
--- worker/watcher.py
+++ worker/watcher.py
@@ -1,4 +1,18 @@
-"""TODO: 
-Redis om jobs binnen te krijgen, en resultaten terug te sturen.
-kraken2 en fasp aanroepen uit python (subprocess) en output ^
-"""
+
+import json
+import time
+import os
+from shared.models import Job
+
+
+def main():
+    """Loop that watches for new jobs and executes them."""
+    jobs_dir = "shared/data"
+    while True:
+        pass # jasses man
+        # time.sleep(3)
+        # for job in os.listdir(jobs_dir):
+            
+
+if __name__ == "__main__":
+    main()
